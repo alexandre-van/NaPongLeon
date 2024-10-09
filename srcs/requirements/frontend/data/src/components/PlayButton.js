3cfd0913a602b471ca84c6a7db05a61a1ad5bec4
amery <alexis.dmery@gmail.com>
2024-10-09 10:12:35 +0200

    matchmaking...

--- srcs/requirements/frontend/data/src/components/PlayButton.js
+++ srcs/requirements/frontend/data/src/components/PlayButton.js
@@ -1,25 +1,34 @@
 import api from '../services/api.js';
 
 const PlayButton = () => {
-    
-    const handlePlayButton = async () => {
-        try {
-            //const response = await api.get('/pong/');
-            const response = await api.get('/game_manager/matchmaking/game_mode=pong_classic');
-            const script = document.createElement('script');
-            script.type = 'module';
-            script.src = response.data.match(/src="([^"]+)"/)[1]; // Extrait le chemin du script
-            document.body.appendChild(script);
-        } catch (error) {  
-            console.log(error.message);
-        }
-    };
+	
+	const handlePlayButton = async () => {
+		try {
+			//const response = await api.get('/pong/');
+			const response = await api.get('/game_manager/matchmaking/game_mode=pong_classic');
+			const gameId = response.data['data']['game_id'];
+            if (!window.gameInfo) {
+                window.gameInfo = {};
+            }
+            window.gameInfo.gameId = gameId;
+			// URL du script que vous souhaitez charger
+			const scriptUrl = 'http://localhost:8080/api/pong/static/pong/main.js'; // Mettez ici votre URL de script
 
-    return (
-        <button onClick={handlePlayButton}>
-            Play Button
-        </button>
-    );
+			// Créer et ajouter le script
+			const script = document.createElement('script');
+			script.type = 'module'; // Si votre script est un module
+			script.src = scriptUrl;
+			document.body.appendChild(script);
+		} catch (error) {  
+			console.log(error.message);
+		}
+	};
+
+	return (
+		<button onClick={handlePlayButton}>
+			Play Button
+		</button>
+	);
 };
 
 export default PlayButton;
--- srcs/requirements/game_manager/src/game_manager/views.py
+++ srcs/requirements/game_manager/src/game_manager/views.py
@@ -1,24 +1,31 @@
+# views.py
 from django.conf import settings
 from django.http import JsonResponse
 from .utils.decorators import auth_required
 from matchmaking.matchmaking import Matchmaking
 from .utils.logger import logger
-from asgiref.sync import sync_to_async
+from rest_framework import status
 import asyncio
 
 @auth_required
 async def get_matchmaking(request, game_mode, username=None):
-	matchmaking_instance = Matchmaking.matchmaking_instance
-	if matchmaking_instance is None:
-		return JsonResponse({"message": "Matchmaking is not initialised"}, status=200)
+    matchmaking_instance = Matchmaking.matchmaking_instance
+    if matchmaking_instance is None:
+        return JsonResponse({"message": "Matchmaking is not initialised"}, status=200)
 
-	logger.debug('get_matchmaking')
-	logger.debug(f'game_mode_request = {game_mode}')
-	logger.debug(f'number of players required  = {settings.GAME_MODES.get(game_mode)}')
-	logger.debug(f'username = {username}')
+    logger.debug('get_matchmaking')
+    logger.debug(f'game_mode_request = {game_mode}')
+    logger.debug(f'number of players required = {settings.GAME_MODES.get(game_mode)}')
+    logger.debug(f'username = {username}')
 
-	# Appel de méthode bloquante avec sync_to_async
-	await sync_to_async(matchmaking_instance.add_player_request)(username, game_mode)
-
-	await asyncio.sleep(10)
-	return JsonResponse({"message": "Matchmaking succeeded"}, status=200)
+    try:
+        # Maintenant nous appelons directement la méthode asynchrone
+        future = await matchmaking_instance.add_player_request(username, game_mode)
+        game_data = await asyncio.wait_for(future, timeout=30)
+        return JsonResponse({"message": "Matchmaking succeeded", "data": game_data}, status=status.HTTP_200_OK)
+    except asyncio.TimeoutError:
+        logger.error(f"Matchmaking timed out for user {username}")
+        return JsonResponse({"message": "Matchmaking timed out"}, status=status.HTTP_504_GATEWAY_TIMEOUT)
+    except Exception as e:
+        logger.error(f"Error in matchmaking for user {username}: {str(e)}")
+        return JsonResponse({"message": "Matchmaking error occurred"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
